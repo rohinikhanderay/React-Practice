65b41a3f22ca788da02d65dfedc6b6f59b3a6856
rohinikhanderay <khanderayrohini@gmail.com>
2022-12-14 10:24:57 +0530

    initial commit

---
+++ src/FooterSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterSection = () => {
+  return (
+    
+    <section class="footer  bg-dark text-white">
+    <p className="lead container">Contact us at Instagram</p>
+    <p className="lead container">You can react via email too</p>
+    <p className="lead container">NO COPYRIGHT</p>
+  </section>
+  
+  )
+}
+
+export default FooterSection;
